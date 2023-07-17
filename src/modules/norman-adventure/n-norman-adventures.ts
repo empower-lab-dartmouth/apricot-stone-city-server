5071ee70b4a2e79d52582f38105ea526ed7d4370
Varuni <vhazra@u.rochester.edu>
2023-07-18 01:16:23 +0530

    8th commit

--- src/modules/norman-adventure/n-norman-adventures.ts
+++ src/modules/norman-adventure/n-norman-adventures.ts
@@ -6,10 +6,11 @@ import normanOnPlane from './cs-on-the-plane'
 import tooManyBags from './cs.too-many-bags'
 import silkRoadHotel from './cs-silk-road-hotel'
 import enteringWorld from './cs-entering-the-world-of-the-self-aware'
+import chatGPTrevolutionizesschool from './cs-chatGPT-revolutionizes-school'
 
 const normanArrivesAtASC = make.module({
     id: 'norman-arrives-at-ASC',
-    submodules: [normanOnPlane, tooManyBags, silkRoadHotel, enteringWorld],
+    submodules: [normanOnPlane, tooManyBags, silkRoadHotel, enteringWorld, chatGPTrevolutionizesschool],
     convoSegments: [],
 })
 
